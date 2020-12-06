6b1d8481f576cad5d20e37747d479024beaef192
Izayomi <74568158+Izayomi@users.noreply.github.com>
2020-12-05 19:41:32 -0500

    Update Lexical Parser.kt

--- Lexical Parser.kt
+++ Lexical Parser.kt
@@ -1,50 +1,75 @@
 fun lexemeTree(s: List<String>): lexNode.exprNode<String> {
     var header = lexNode.exprNode<String>("")
     var current = header
+
+    if (s[0] != "LPAREN") {
+        var parentNode = lexNode.exprNode<String>(s[1])
+        var valNode = lexNode.numNode<String>(s[0])
+        header = parentNode
+        header.setLhs (valNode)
+    }
+
     for (i in 0 until s.size) {
         when (s[i]) {
             "PLUS" -> {
                 var newNode = lexNode.exprNode<String>(s[i])
                 newNode.setLhs(header)
+                header.setPar (newNode)
                 header = newNode
                 current = newNode
             }
             "MINUS" -> {
                 var newNode = lexNode.exprNode<String>(s[i])
                 newNode.setLhs(header)
+                header.setPar (newNode)
                 header = newNode
                 current = newNode
             }
             "TIMES" -> {
                 var newNode = lexNode.exprNode<String>(s[i])
-                if (current.value != "POWER") {
+                if (current.value == "POWER" || current.value == "TIMES" || current.value == "DIVIDES") {
+                    var parentNode = current.getPar ()
+                    parentNode?.setRhs (newNode)
+                    newNode.setLhs (current)
+                    current.setPar (newNode)
+                    newNode.setPar (parentNode)
+                } else {
                     newNode.setLhs (current.getRhs ())
                     current.setRhs (newNode)
-                } else {
-                    var parentNode = current.getPar ()
-                    newNode.setLhs (current)
-                    newNode.setPar (parentNode)
-                    parentNode.setRhs (newNode)
+                    newNode.setPar (current)
                 }
                 current = newNode
             }
             "DIVIDES" -> {
                 var newNode = lexNode.exprNode<String>(s[i])
-                if (current.value != "POWER") {
+                if (current.value == "POWER" || current.value == "TIMES" || current.value == "TIMES") {
+                    var parentNode = current.getPar ()
+                    parentNode?.setRhs (newNode)
+                    newNode.setLhs (current)
+                    current.setPar (newNode)
+                    newNode.setPar (parentNode)
+                } else {
                     newNode.setLhs (current.getRhs ())
                     current.setRhs (newNode)
-                } else {
-                    var parentNode = current.getPar ()
-                    newNode.setLhs (current)
-                    newNode.setPar (parentNode)
-                    parentNode.setRhs (newNode)
+                    newNode.setLhs (current.getRhs ())
+                    current.setRhs (newNode)
+                    newNode.setPar (current)
                 }
                 current = newNode
             }
             "POWER" -> {
                 var newNode = lexNode.exprNode<String>(s[i])
-                newNode.setLhs (current.getRhs ())
-                current.setRhs (newNode)
+                if (current.value == "POWER") {
+                    var parentNode = current.getPar ()
+                    parentNode?.setRhs (newNode)
+                    newNode.setLhs (current)
+                    current.setPar (newNode)
+                    newNode.setPar (parentNode)
+                } else {
+                    newNode.setLhs (current.getRhs ())
+                    current.setRhs (newNode)
+                    newNode.setPar (current)
+                }
                 current = newNode
             }
             "LPAREN" -> {
@@ -58,6 +83,7 @@ fun lexemeTree(s: List<String>): lexNode.exprNode<String> {
                 }
                 var newNode = lexemeTree(recursiveList)
                 current.setRhs(newNode)
+                newNode.setPar (current)
                 current = newNode
             }
             "PI" -> {
